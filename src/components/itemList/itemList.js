75a40e77c5066e3aa71583f5ebc155069d7a04e6
maxim-js <makssp7@inbox.ru>
2020-04-22 13:51:50 +0800

    first commit

---
+++ src/components/itemList/itemList.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import './itemList.css';
+import Spinner from '../spinner';
+import gotService from '../../services/gotService';
+
+class ItemList extends Component {
+
+
+    renderItems(arr) {
+        return arr.map((item) => {
+            const {id} = item;
+
+            const label = this.props.renderItem(item);
+
+            return (
+                <li 
+                    key={id}
+                    className="list-group-item"
+                    onClick={ () => this.props.onItemSelected(id)}>
+                    {label}
+                </li>
+            )
+        })
+    }
+
+    render() {
+        const {data} = this.props;
+        const items = this.renderItems(data);
+
+        return (
+            <ul className="item-list list-group">
+                {items}
+            </ul>
+        );
+    }
+}
+
+const withData = (View, getData) => {
+    return class extends Component {
+
+        state = {
+            data: null
+        }
+    
+        componentDidMount() {
+    
+            getData()
+                .then( (data) => {
+                    this.setState({
+                        data
+                    })
+                })
+        }
+    
+        render() {
+            
+            const {data} = this.state;
+
+            if (!data) {
+                return <Spinner/>
+            }
+            return <View {...this.props} data={data}/>
+        }
+    }
+}
+
+const {getAllCharacters} = new gotService();
+export default withData(ItemList, getAllCharacters);
